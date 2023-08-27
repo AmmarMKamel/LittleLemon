9022e3b5b406d12507a728be4193d5cb46fba4fb
Ammar M. Kamel <ammarkamel9899@gmail.com>
2023-08-27 10:32:50 +0300

    Create seralizers and views

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Menu
 
 class UserSerializer(serializers.ModelSerializer):
     model = User
     fields = ['url', 'username', 'email', 'groups']
 
 class MenuItemSerializer(serializers.ModelSerializer):
-    pass
+    model = Menu
+    fields = ['id', 'title', 'price', 'inventory']
--- restaurant/views.py
+++ restaurant/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import generics
 from .serializers import MenuItemSerializer
+from .models import Menu
 # Create your views here.
 def index(request):
     return render(request, 'home.html', {})
 
 class MenuItemsView(generics.ListCreateAPIView):
-    # queryset = MenuItem.objects.all()
+    queryset = Menu.objects.all()
     serializer_class = MenuItemSerializer
 
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
-    # queryset = MenuItem.objects.all()
+    queryset = Menu.objects.all()
     serializer_class = MenuItemSerializer
